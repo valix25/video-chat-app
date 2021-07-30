e59a13317daf4030ba7dc18f65a5a827d7cf5a5f
Valentin Vasiliu <vali15tnt51v@gmail.com>
2021-07-30 18:11:45 +0200

    Show own video in browser

--- public/script.js
+++ public/script.js
@@ -1,8 +1,20 @@
 const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
 });
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
 
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
@@ -11,3 +23,13 @@ myPeer.on("open", (id) => {
 socket.on("user-connected", (userId) => {
   console.log("User connected: " + userId);
 });
+
+function addVideoStream(video, stream) {
+  // this will allow us to play our video
+  video.srcObject = stream;
+  // once the stream is loaded and the video is loaded on our page, play that video
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
